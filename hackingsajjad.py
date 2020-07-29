1c940b39d234c4c4e47911c98c7d1f05804013aa
Sajjad-Mohammad-Official <68941850+Sajjad-Mohammad-Official@users.noreply.github.com>
2020-07-29 12:49:33 +0600

    Update and rename kingboy.py to hackingsajjad.py

--- hackingsajjad.py
+++ hackingsajjad.py
@@ -100,7 +100,7 @@ jalan("\033[1;93m   ┈┈┈┈┈┈┈┈╲┊┊┊┊╱┈┈┈┈┈┈
 jalan("\033[1;93m   ┈┈┈┈┈┈┈┈▕╲▂▂╱▏┈┈┈┈┈┈┈┈")
 print "\033[1;93m♡─────╱▔▔▔▔┊┊┊┊▔▔▔▔╲───────♡\033[1;96mLogin RanaAhil\033[1;95m♡╰──────────•◈•──────────╯♡"
 
-CorrectUsername = "sajjad"
+CorrectUsername = "Sajjad"
 CorrectPassword = "sajjad"
 
 loop = 'true'
